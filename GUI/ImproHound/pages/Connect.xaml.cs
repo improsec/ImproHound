fa267b5c3203dd83d3934095ff61ccb5023d9934
ADNinja <jbk@improsec.com>
2021-02-06 00:16:32 +0100

    Make the connection page look pretty

---
+++ GUI/ImproHound/pages/Connect.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ImproHound.pages
+{
+    public partial class ConnectPage : Page
+    {
+        public ConnectPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DBConnection greeter = new DBConnection("bolt://localhost:7687", "neo4j", "bloodhound");
+            await greeter.asyncAsync("CREATE (a:Greeting) SET a.message = \"hi\" RETURN a.message");
+        }
+    }
+}
