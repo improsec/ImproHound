254a68f9770a9a7a985327f2b76a2da059f58805
ADNinja <jbk@improsec.com>
2021-02-05 21:59:31 +0100

    Change GUI project to be WPF instead of WinForms

---
+++ GUI/ImproHound/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImproHound
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DBConnection greeter = new DBConnection("bolt://localhost:7687", "neo4j", "bloodhound");
+            await greeter.asyncAsync("CREATE (a:Greeting) SET a.message = \"hi\" RETURN a.message");
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
