732da6f606d3f507acca803d686588bd76b460d5
ADNinja <jbk@improsec.com>
2021-02-07 23:01:30 +0100

    Put timeout on connection to DB and make disable mouse click while trying to connect

--- GUI/ImproHound/pages/Connect.xaml.cs
+++ GUI/ImproHound/pages/Connect.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Neo4j.Driver;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using Neo4j.Driver;
+using System.Windows.Input;
 
 namespace ImproHound.pages
 {
@@ -16,36 +18,71 @@ namespace ImproHound.pages
             this.containerWindow = containerWindow;
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
         {
-            // Make sure we can connect to the DB and the graph is not empty
             object output;
+            int timeout = 5000;
+            Mouse.OverrideCursor = Cursors.Wait;
+            url.IsEnabled = false;
+            username.IsEnabled = false;
+            password.IsEnabled = false;
+            connectButton.IsEnabled = false;
+
+            // Make sure we can connect to the DB and the graph is not empty
             DBConnection connection = new DBConnection(url.Text, username.Text, password.Text);
+
             try
             {
-                List<IRecord> testConnection = await connection.CypherQuery("MATCH (n) RETURN COUNT(n)");
-
-                if (!testConnection[0].Values.TryGetValue("COUNT(n)", out output))
+                Task<List<IRecord>> task = connection.CypherQuery("MATCH (n) RETURN COUNT(n)");
+                if (await Task.WhenAny(task, Task.Delay(timeout)) == task)
                 {
-                    MessageBox.Show("Something went wrong.\nNo authentication error but could not fetch number of nodes in graph.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                    return;
-                }
+                    List<IRecord> testConnection = await task;
+                    if (!testConnection[0].Values.TryGetValue("COUNT(n)", out output))
+                    {
+                        // Unknown error
+                        MessageBox.Show("Something went wrong.\nNo authentication error but could not fetch number of nodes in graph.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        SetDefaultControls();
+                        return;
+                    }
 
-                long numOfNodes = (long)output;
-                Console.WriteLine("Number of nodes in graph: " + numOfNodes);
-                if (numOfNodes.Equals(0))
+                    long numOfNodes = (long)output;
+                    Console.WriteLine("Number of nodes in graph: " + numOfNodes);
+                    if (numOfNodes.Equals(0))
+                    {
+                        // 0 nodes in graph error
+                        MessageBox.Show("You have 0 nodes in your graph.\nMake sure you have upload BloodHound data to graph before connecting.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        SetDefaultControls();
+                        return;
+                    }
+
+                } else
                 {
-                    MessageBox.Show("You have 0 nodes in your graph.\nMake sure you have upload BloodHound data to graph before connecting.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    // Timeout error
+                    MessageBox.Show("No response in " + timeout + " ms.\nVerify DB URL and DB is running.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    SetDefaultControls();
                     return;
                 }
 
             } catch (AuthenticationException err)
             {
+                // Auth error
                 MessageBox.Show("Authentication error:\n" + err.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                SetDefaultControls();
+                return;
             }
 
             // Jump to OU structure page
             containerWindow.NavigateToPage(new OUStructurePage());
+            SetDefaultControls();
+        }
+
+        private void SetDefaultControls()
+        {
+            Mouse.OverrideCursor = null;
+            url.IsEnabled = true;
+            username.IsEnabled = true;
+            password.IsEnabled = true;
+            connectButton.IsEnabled = true;
         }
     }
 }
